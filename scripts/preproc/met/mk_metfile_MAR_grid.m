e40a77caff6284481db7aec1578b0152eab0ee0f
mgcooper <mguycooper@gmail.com>
2022-09-20 16:06:16 -0700

    test new makeMarMetfileGrid option to use native mar grid cells

--- scripts/preproc/met/mk_metfile_MAR_grid.m
+++ scripts/preproc/met/mk_metfile_MAR_grid.m
@@ -1,18 +1,12 @@
 clean
 
-% the only difference b/w this and the _point script is the wrapper around
-% a list of points. this script replaces build_MAR_region_metfiles by using
-% the raw mar nc files and geus albedo files rather than the processed
-% files
-
 % NOTE: when I ran this on my personal mac I should have used xmask/ymask
 % not xmodel/ymodel to get the runs on the same numbering as the original
 % runs which was xmask/ymask. the mistake was in using list=dir() in THIS
 % script to save the data, now that mistake is propagated into the new runs
 % so i'll just have to keep using xmodel/ymodel
 
-savedata    =  true;
-modis       =  true;
+savedata    =  false;
 sitename    =  'region';
 yri         =  2008;
 yrf         =  2018;
@@ -24,31 +18,29 @@ newdt       =  '15m';
 % set paths
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-% starting with hills data, I am only saving in the icemodel input folder
-% pathdata =  '/Users/coop558/mydata/mar3.11/RUH2/';
-% pathsave =  '/Users/coop558/mydata/mar3.11/matfiles/region/level2/test/';
-% pathsupp =  '/Users/coop558/mydata/geus/albedo/raw/';
-
-pathdata =  '/Volumes/Samsung_T5b/mar3.11/RUH2/';
-pathsave =  '/Users/mattcooper/data/mar3.11/matfiles/region/level2/grids/';
-pathsupp =  '/Volumes/Samsung_T5b/geus/albedo/';
-list     =  getlist(pathdata,'*.nc');
+pathdata    =  setpath('mar3.11','data');
+pathsave    =  setpath('mar3.11/matfiles/region/level2/grids/','data');
+pathmodis   =  setpath('geus/albedo/gris/','data');
+filelist    =  getlist(pathdata,'*.nc');
 
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 % read in the catchment to get the x,y coordinate 
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-load('projsipsn.mat')
 
-% for the gridded region runs, we want the x,y coordinates to be the bare
-% ice mask
-load('modis_ice_mask.mat');
+% load data
+load('projsipsn.mat','projsipsn')         % sea ice polar stereographic north
+load('modis_ice_mask.mat','icemask');     % bare ice mask x,y coordinates
+load('imbie_SW.mat','imbie');             % imbie sectors
 
+% use the bare ice mask x,y coord's to build sector met files
 Xmet     = icemask.xmodel;
 Ymet     = icemask.ymodel;
 
+% use the sw sector as an ROI mask
+ROI      = imbie.poly;
 
 % some variables need only be extracted once, do them first
-fmar     = [pathdata 'MARv3.11-ERA5-15km-' num2str(yri) '.nc'];
+fmar     = [pathdata 'MARv3.11-ERA5-15km-2018.nc'];
 info     = ncparse(fmar);
 LON      = ncread(fmar,'LON');
 LAT      = ncread(fmar,'LAT');
@@ -60,31 +52,40 @@ VAR      = ncread(fmar,'RUH');
 % figure; scatter(Xmar(:),Ymar(:)); hold on; scatter(Xmet,Ymet);
 
 % similarly, do the modis conversion from lat/lon to x/y first
-fmodis   = [pathsupp 'Greenland_Reflectivity_' num2str(yri) '_5km_C6.nc'];
+fmodis   = [pathmodis 'Greenland_Reflectivity_' num2str(yri) '_5km_C6.nc'];
 LON      = double(ncread(fmodis,'lon'));
 LAT      = double(ncread(fmodis,'lat'));
 [Xmod, ...
    Ymod] = projfwd(projsipsn,LAT,LON);
 
+% for testing:
+n=nyears;
+
 for n = 1:nyears
     
    yyyy    = yri+n-1;
    fmar    = [pathdata 'MARv3.11-ERA5-15km-' num2str(yyyy) '.nc'];
-   fmodis  = [pathsupp 'Greenland_Reflectivity_' num2str(yyyy) '_5km_C6.nc'];
+   fmodis  = [pathmodis 'Greenland_Reflectivity_' num2str(yyyy) '_5km_C6.nc'];
    
    % for the region gridded runs, use the simple naming convention
    fsavemet    = [pathsave 'met_' int2str(yyyy) '.mat'];
-   if ~exist(savepath,'dir'); mkdir(savepath); end
+   
+%    commented out for testing
+%    if ~exist(savepath,'dir'); mkdir(savepath); end
       
    % the filename gets appended with the point number in the function
-   fsavedata   = [pathsave 'data_' int2str(yyyy) '.mat'];
+   fsavedata   =  [pathsave 'data_' int2str(yyyy) '.mat'];
 
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         
-   [met,data]  = makeMarMetfileGrid(fmar,Xmar,Ymar,Xmet,Ymet,yyyy,      ...
-                     'modis',modis,'fmodis',fmodis,'Xmodis',Xmod,       ...
-                     'Ymodis',Ymod,'newdt',newdt);
-        
+   [met,data]  =  makeMarMetfileGrid(fmar,'Xmar',Xmar,'Ymar',Ymar,      ...
+                  'ROI',ROI,'Xmet',Xmet,'Ymet',Ymet,'fmodis',fmodis,    ...
+                  'Xmodis',Xmod,'Ymodis',Ymod);
+               
+
+   [met,data]  =  makeMarMetfileGrid(fmar,'Xmar',Xmar,'Ymar',Ymar,      ...
+                  'ROI',ROI,'fmodis',fmodis,'Xmodis',Xmod,'Ymodis',Ymod);
+
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
    if savedata == true
