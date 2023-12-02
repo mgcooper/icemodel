a89e2031bb8d22873832879d3a9539059cf9776a
mgcooper <mguycooper@gmail.com>
2023-12-02 10:24:19 -0800

    refactor(SFCFLIN): Rename SFCFLIN2 SFCFLIN.
    Clean up SEB linearization functions.
    
    The higher-order stability linearization was originally SFCFLIN.
    
    The simpler one that works was SFCFLIN2, now SFCFLIN.

--- functions/SFCFLIN.m
+++ functions/SFCFLIN.m
@@ -1,26 +1,26 @@
-function [Sc,Sp] = SFCFLIN(Tair,Qsi,Qli,ea,albedo,De,Pa,wspd,cv_air,emiss, ...
-      SB,roL,Tf,chi,Tsfc,scoef,liqflag)
-   %SFCFLIN Linearize the surface energy balance equation w/form S = Sc + Sp*T
-
-   % NOTE: all terms here are 'old', meaning these are computed once at the
-   % start of the timestep, and on iterations updated as S = Sc + Sp*T_new
-
-   % the linearization is of the form: SEB = SEB'+dSEB/dTsfc(Tsfc-Tsfc')
-
-   % note on solar radiation source term. the penetrating solar radiation
-   % absorbed in each cv (dQp) that comes out of updateextcoefs is scaled to
-   % match the total subsurface absorbed solar radiation at each timestep in
-   % SOLAR_HEAT (dQp_new = -(1-chi)*Qsi/total_solar).*dQp_old). Although not
-   % intuitive, just trust that this provides the amount of solar radiation
-   % absorbed in each cv AFTER removing chi*Qsi*1-albedo from Qsi. TLDR: as
-   % long as the SEB term gets chi*(1.0-albedo) * Qsi, and the Qsip that comes
-   % out of SOLARPEN and goes into SOLAR_HEAT uses the same chi, then energy
-   % is not double counted. The 'chi' part goes into the skin, and the rest
-   % goes into each cv. So, in the SEB linearization, we don't need any
-   % special procedures to deal with the part absorbed in the top layer. The
-   % equation below puts the skin part into the SEB linearization, and the
-   % heat solver puts in the cv part, and they are combined in the equation
-   % for for the top layer/node.
+function [Sc, Sp] = SFCFLIN(Ta, Qsi, Qli, albedo, wspd, Pa, De, ...
+      ea, cv_air, emiss, SB, roL, scoef, chi, Tf, Ts, liqflag)
+   %SFCFLIN Linearize the surface energy balance equation
+   %
+   % The linearization is of the form: S = Sc + Sp * T 
+   % 
+   % To construct the linearization, the SEB is separated into terms that are
+   % independent of Ts (call them S_0) and terms that depend on Ts. The
+   % latter are linearized using truncated Taylor expansions and resulting 
+   % terms which are independent of the current Ts (i.e., those that depend on
+   % past values of Ts) are grouped with the S_0 terms to form S'.
+   % 
+   % SEB = SEB' + dSEB/dTs (Ts-Ts')
+   %
+   % Note that the outgoing longwave and the saturation vapor pressure are
+   % linearized around T_old but the stability function is not linearized,
+   % thus T_old should be used to compute the stability function.
+   %
+   % All terms passed here are 'old', meaning the linearizations are computed
+   % once at the start of the timestep, and on iterations updated as 
+   % S = Sc + Sp * T_new 
+   %
+   % See also: 
 
    if liqflag == true
       % Over water.
@@ -35,56 +35,39 @@ function [Sc,Sp] = SFCFLIN(Tair,Qsi,Qli,ea,albedo,De,Pa,wspd,cv_air,emiss, ...
    end
 
    % Compute the constants used in the stability coefficient computations
-   B1 = scoef(2)/(Tair*wspd^2);
-   B2 = scoef(3)/(sqrt(Tair)*wspd);
+   B1 = scoef(2) / (Ta * wspd ^ 2);
+   B2 = scoef(3) / (sqrt(Ta) * wspd);
 
-   % This accounts for an increase in turbulent fluxes under unstable conditions.
-   es0 = A*exp((B*(Tsfc-Tf))/(C+(Tsfc-Tf)));
-   AS = B*C*es0/(C-(Tsfc-Tf));
+   % Compute saturation vapor pressure
+   es = A * exp(B * (Ts - Tf) / (C + Ts - Tf));
 
-   if (Tsfc>Tair)         % Unstable case.
-      B3 = 1 + B2*sqrt(Tsfc-Tair);
-      S = 1 + B1*(Tsfc-Tair)/B3;
-      dSdT = B1/B3 - B1*B2*(Tsfc-Tair)/(2*B3^2*sqrt(Tsfc-Tair));
-   elseif (Tsfc<Tair)     % Stable case.
-      S = 1 /(1 + B1/2*(Tair-Tsfc))^2;
-      dSdT = B1/(1 + B1/2*(Tair-Tsfc))^3;
-   else                    % Neutrally stable case.
+   % This accounts for an increase in turbulent fluxes under unstable conditions
+   if Ts > Ta
+      B3 = 1 + B2 * sqrt(Ts - Ta);
+      S = 1 + B1 * (Ts - Ta) / B3;
+   elseif Ts < Ta
+      S = 1 / (1 + B1 / 2 * (Ta - Ts)) ^ 2;
+   else
       S = 1.0;
-      dSdT = 0.0;
    end
 
-   %% linearizations
+   % linearizations
 
-   % Note that Qc = 0 for Nuemann bc.
-
-   % outgoing longwave:
-   Sc_Qle = 3*emiss*SB*Tsfc^4;
-   Sp_Qle = -4/3*Sc_Qle/Tsfc;
+   % outgoing longwave (note: -Qle = -emiss*SB*Ts^4):
+   Sc_Qle = 3 * emiss * SB * Ts ^ 4;
+   Sp_Qle = -4 / 3 * Sc_Qle / Ts;
 
    % sensible heat flux:
-   Sc_Qh = cv_air*De*(Tsfc*dSdT*(Tsfc-Tair) + Tair*S);
-   Sp_Qh = cv_air*De*(dSdT*(Tair-Tsfc)-S);
+   Sc_Qh = cv_air * De * S * Ta;
+   Sp_Qh = -cv_air * De * S;
 
    % latent heat flux:
-   Sc_Qe = roL*De*0.622/Pa*(Tsfc*(dSdT*(es0-ea)+S*es0*AS)+S*(ea-es0));
-   Sp_Qe = roL*De*0.622/Pa*(dSdT*(ea-es0)-S*es0*AS);
+   Sc_Qe = roL * De * 0.622 / Pa * S ...
+      * (ea - es * (1 - B * C * Ts / (C + Ts - Tf) ^ 2));
+   Sp_Qe = -roL * De * 0.622 / Pa * S ...
+      * es * B * C / (C + Ts - Tf) ^ 2;
 
    % combine net sw, incoming lw, conduction, and snow/rain heat flux:
-   Sc = Sc_Qle + Sc_Qh + Sc_Qe + chi*Qsi*(1.0-albedo) + emiss*Qli;
+   Sc = Sc_Qle + Sc_Qh + Sc_Qe + emiss * Qli + chi * Qsi * (1.0 - albedo);
    Sp = Sp_Qle + Sp_Qh + Sp_Qe;
-
-   % might need to add a check for switch from stable/unstable within a step
-   % check if Sp_Qh or Sp_Qe should be negative
-
-   % % for testing
-   % Qr = EEE - emiss*SB.*xTsfc.^4;
-   % Qh = AAA.*Sfnc(Tair,xTsfc,wspd,grav,kapp,z_0,z_obs).*(Tair-xTsfc)
-   % Qe = FFF.*CCC.*Sfnc(Tair,xTsfc,wspd,grav,kapp,z_0,z_obs).*(ea-Vfnc(xTsfc,Tf))
-   %
-   % % total heat = net radiation + sensible + latent
-   % Q = Qr + Qh + Qe
-
-   % don't think this is complete might have been distracted before finishing
-   % SEB = EEE+3*emiss*SB*xTsfc^4+Sc_Qh+Sc_Qe+(-4*emiss*SB*T_old(1)^3-Sp_Q
 end
