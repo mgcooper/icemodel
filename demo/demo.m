771d7fcf78d4e152b4936e576f4a43667b6150e2
mgcooper <mguycooper@gmail.com>
2026-03-01 12:59:18 -0800

    refactor(demo): Move examples to a switch-case to choose one.

--- demo/demo.m
+++ demo/demo.m
@@ -31,78 +31,89 @@ setup()
 % Source the project-level configuration.
 config = icemodel.config('casename', 'demo');
 
-%% Example 1: KAN-M weather station in 2016
+%% Set the case
+runcase = 1;
 
-% Set the run configuration.
-saveflag = false;
-sitename = 'kanm';
-forcings = 'kanm';
-smbmodel = 'skinmodel';
-simyears = 2016:2016;
-backupflag = false;
+switch runcase
 
-% Run the model.
-[ice1, ice2, met, opts] = icemodel.run.point(...
-   "saveflag", saveflag, ...
-   "sitename", sitename, ...
-   "forcings", forcings, ...
-   "smbmodel", smbmodel, ...
-   "simyears", simyears, ...
-   "backupflag", backupflag);
+   case 1
 
-% Create the demo plot comparing the simulated surface energy balance to the
-% observed values from the KANM weather station.
-icemodel.plot.enbal(ice1, met)
+      % Example 1: KAN-M weather station in 2016
 
-%% Example 2 - Replace KAN-M albedo with MODIS albedo
+      % Set the run configuration.
+      saveflag = false;
+      sitename = 'kanm';
+      forcings = 'kanm';
+      smbmodel = 'icemodel';
+      simyears = 2016:2016;
+      backupflag = false;
 
-% Set the run configuration. Use the "userdata" option to swap the albedo.
-saveflag = false;
-sitename = 'kanm';
-forcings = 'kanm';
-smbmodel = 'skinmodel';
-userdata = 'modis';
-uservars = 'albedo';
-simyears = 2016:2016;
-backupflag = false;
+      % Run the model.
+      [ice1, ice2, met, opts] = icemodel.run.point(...
+         "saveflag", saveflag, ...
+         "sitename", sitename, ...
+         "forcings", forcings, ...
+         "smbmodel", smbmodel, ...
+         "simyears", simyears, ...
+         "backupflag", backupflag);
 
-% Run the model
-[ice1, ice2, met, opts] = icemodel.run.point(...
-   "saveflag", saveflag, ...
-   "sitename", sitename, ...
-   "forcings", forcings, ...
-   "smbmodel", smbmodel, ...
-   "simyears", simyears, ...
-   "userdata", userdata, ...
-   "uservars", uservars, ...
-   "backupflag", backupflag);
+      % Create the demo plot comparing the simulated surface energy balance to
+      % the observed values from the KANM weather station.
+      icemodel.plot.enbal(ice1, met)
 
-% Demo plot
-icemodel.plot.enbal(ice1, met)
+   case 2
 
-%% Example 3 - Replace KAN-M air temperature with MERRA-2 air temperature
+      % Example 2 - Replace KAN-M albedo with MODIS albedo
 
-% Set the run configuration. Use the "userdata" option to swap the air temp.
-saveflag = false;
-sitename = 'kanm';
-forcings = 'kanm';
-smbmodel = 'skinmodel';
-userdata = 'merra';
-uservars = 'tair';
-simyears = 2016:2016;
-backupflag = false;
+      % Set the run configuration. Use the "userdata" option to swap the albedo.
+      saveflag = false;
+      sitename = 'kanm';
+      forcings = 'kanm';
+      smbmodel = 'skinmodel';
+      userdata = 'modis';
+      uservars = 'albedo';
+      simyears = 2016:2016;
+      backupflag = false;
 
-% Run the model
-[ice1, ice2, met, opts] = icemodel.run.point(...
-   "saveflag", saveflag, ...
-   "sitename", sitename, ...
-   "forcings", forcings, ...
-   "smbmodel", smbmodel, ...
-   "simyears", simyears, ...
-   "userdata", userdata, ...
-   "uservars", uservars, ...
-   "backupflag", backupflag);
+      % Run the model
+      [ice1, ice2, met, opts] = icemodel.run.point(...
+         "saveflag", saveflag, ...
+         "sitename", sitename, ...
+         "forcings", forcings, ...
+         "smbmodel", smbmodel, ...
+         "simyears", simyears, ...
+         "userdata", userdata, ...
+         "uservars", uservars, ...
+         "backupflag", backupflag);
 
-% Demo plot
-icemodel.plot.enbal(ice1, met)
+      % Demo plot
+      icemodel.plot.enbal(ice1, met)
 
+   case 3
+
+      % Example 3 - Replace KAN-M air temperature with MERRA-2 air temperature
+
+      % Set the run configuration. Use "userdata" option to swap the air temp.
+      saveflag = false;
+      sitename = 'kanm';
+      forcings = 'kanm';
+      smbmodel = 'skinmodel';
+      userdata = 'merra';
+      uservars = 'tair';
+      simyears = 2016:2016;
+      backupflag = false;
+
+      % Run the model
+      [ice1, ice2, met, opts] = icemodel.run.point(...
+         "saveflag", saveflag, ...
+         "sitename", sitename, ...
+         "forcings", forcings, ...
+         "smbmodel", smbmodel, ...
+         "simyears", simyears, ...
+         "userdata", userdata, ...
+         "uservars", uservars, ...
+         "backupflag", backupflag);
+
+      % Demo plot
+      icemodel.plot.enbal(ice1, met)
+end
